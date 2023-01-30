4404df06817512829ceb00f5976ea0fff4909207
Cogita (David) <99983918+Iamcogita@users.noreply.github.com>
2023-01-30 14:30:47 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import Words.WordHistogram;
 public class Main {
     public static void main(String[] args) {
 
-        final String STRING_TO_ANALYSE = "You can always trust the untrustworthy because you can always trust that they will be untrustworthy. It's the trustworthy you cannot trust.";
+        final String STRING_TO_ANALYSE = "Your String here.";
 
         WordHistogram wordHistogram = new WordHistogram();
         wordHistogram.analyseString(STRING_TO_ANALYSE);
